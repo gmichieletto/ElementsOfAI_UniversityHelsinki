4ff44fb63a01d5a110bdc9342580548471fd203c
gmichieletto <gmichieletto@users.noreply.github.com>
2021-02-17 14:02:19 +0100

    Update Chapter1Exercise2.py

--- Chapter1Exercise2.py
+++ Chapter1Exercise2.py
@@ -1,1 +1,51 @@
+portnames = ["PAN", "AMS", "CAS", "NYC", "HEL"]
+
+# https://sea-distances.org/
+# nautical miles converted to km
+
+D = [
+        [0,8943,8019,3652,10545],
+        [8943,0,2619,6317,2078],
+        [8019,2619,0,5836,4939],
+        [3652,6317,5836,0,7825],
+        [10545,2078,4939,7825,0]
+    ]
+
+# https://timeforchange.org/co2-emissions-shipping-goods
+# assume 20g per km per metric ton (of pineapples)
+
+co2 = 0.020
+
+# DATA BLOCK ENDS
+
+# these variables are initialised to nonsensical values
+# your program should determine the correct values for them
+smallest = 1000000
+bestroute = [0, 0, 0, 0, 0]
+
+def permutations(route, ports):
+    # write the recursive function here
+    # remember to calculate the emissions of the route as the recursion ends
+    # and keep track of the route with the lowest emissions
+    global smallest, bestroute
+    if len(ports) < 1:
+        score = co2 * D[route[0]][route[1]] + D[route[1]][route[2]] + D[route[2]][route[3]] + D[route[3]][route[4]]
+        #dist = co2 * sum(D[i][j] for i, j in zip(route[:-1], route[1:]))
+        if score < smallest:
+            smallest = score
+            bestroute = route
+    else:
+        for i in range(len(ports)):
+            permutations(route+[ports[i]], ports[:i]+ports[i+1:])
+ 
+permutations([0], list(range(1, len(portnames))))
+
+def main():
+    # this will start the recursion 
+    permutations([0], list(range(1, len(portnames))))
+
+    # print the best route and its emissions
+    print(' '.join([portnames[i] for i in bestroute]) + " %.1f kg" % smallest)
+
+main()
 
