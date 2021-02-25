c74632c908ee5b80a6501d47b87486b625a9fbd8
gmichieletto <gmichieletto@users.noreply.github.com>
2021-02-25 13:40:35 +0100

    Create Exercise17.py

---
+++ Exercise17.py
@@ -0,0 +1,25 @@
+# this data here is the bag of words representation of This Little Piggy
+data = [[1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 0, 1, 3, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1]]
+
+def distance(row1, row2):
+    # replace the following by a function that returns the sum of differences between the
+    # words in row1 and row2. this is the Manhattan distance.
+    # you can assume that row1 and row2 are list with equal length, containing numeric values.
+    sum = 0
+    for a, b in zip(row1, row2):
+        sum += abs(a-b)
+    return sum
+
+def all_pairs(data):
+    # this calculates the distances between all sentence pairs in the data
+    # you do not need to change this
+    dist = [[distance(sent1, sent2) for sent1 in data] for sent2 in data]
+    print(dist)
+
+all_pairs(data)
+
+print("---------------advanced----------------")
