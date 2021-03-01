a8c3871854140317ddd97584fbc9b68d6404e713
gmichieletto <gmichieletto@users.noreply.github.com>
2021-03-01 11:15:53 +0100

    Create Exercise19.py

---
+++ Exercise19.py
@@ -0,0 +1,20 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.datasets import make_moons
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+# do not edit this
+# create fake data
+x, y = make_moons(
+    n_samples=500,  # the number of observations
+    random_state=42,
+    noise=0.3
+)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+
+# Create a classifier and fit it to our data
+knn = KNeighborsClassifier(n_neighbors=133)
+knn.fit(x_train, y_train)
+
+print("training accuracy: %f" % 0.0)
+print("testing accuracy: %f" % 0.0)
